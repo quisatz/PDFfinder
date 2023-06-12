d8d4e779197b8c7194529d498dd50cbd8e29690a
quisatz <borysgolebiowskipl@gmail.com>
2023-06-12 20:45:27 +0200

    changed descriptions of widgets to variables and temporarily withdrawn styles

--- gui/feedback.py
+++ gui/feedback.py
@@ -5,48 +5,48 @@ from tkinter import ttk, messagebox
 class PDFFinderFeedback(tkk.Toplevel):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
-
-        bg_color = '#0088FF'
+        self.short = self.master.languages
+        #bg_color = '#0088FF'
 
         self.grab_set()
 
-        self.title('Feedback')
+        self.title(self.short["txt_feedback_screen_feedback"])
         self.resizable(False, False)
 
-        self.configure(bg=bg_color)
+        #self.configure(bg=bg_color)
 
         self.frame_header = ttk.Frame(self)
         self.frame_header.pack()
 
         self.frame_header.style = ttk.Style()
-        self.frame_header.style.configure('TFrame', background='#0088FF')
-        self.frame_header.style.configure('TButton', background='#0088FF')
-        self.frame_header.style.configure('TLabel', background='#0088FF', font=('Arial', 10))
+        # self.frame_header.style.configure('TFrame', background='#FFFFFF')
+        # self.frame_header.style.configure('TButton', background='#FFFFFF')
+        # self.frame_header.style.configure('TLabel', background='#FFFFFF', font=('Arial', 10))
         self.frame_header.style.configure('Header.TLabel', font=('Arial', 14))
 
         self.logo = tkk.PhotoImage(file='logoOpnie.png')
         ttk.Label(self.frame_header, image=self.logo).grid(row=0, column=0, rowspan=2)
-        ttk.Label(self.frame_header, text='Zostaw swoją opinię.', style='Header.TLabel', background=bg_color).grid(
+        ttk.Label(self.frame_header, text=self.short["txt_feedback_screen__leave_your_opinion"], style='Header.TLabel').grid(
             row=0, column=1)
 
         self.frame_content = ttk.Frame(self)
         self.frame_content.pack()
 
-        ttk.Label(self.frame_content, text='Name:').grid(row=0, column=0, padx=5, sticky='sw')
-        ttk.Label(self.frame_content, text='Email:').grid(row=0, column=1, padx=5, sticky='sw')
-        ttk.Label(self.frame_content, text='Comments:').grid(row=2, column=0, padx=5, sticky='sw')
+        ttk.Label(self.frame_content, text=self.short["txt_feedback_screen__name"]).grid(row=0, column=0, padx=5, sticky='sw')
+        ttk.Label(self.frame_content, text=self.short["txt_feedback_screen__email"]).grid(row=0, column=1, padx=5, sticky='sw')
+        ttk.Label(self.frame_content, text=self.short["txt_feedback_screen__comment"]).grid(row=2, column=0, padx=5, sticky='sw')
 
-        self.entry_name = ttk.Entry(self.frame_content, width=24, font=('Arial', 10), background=bg_color)
-        self.entry_email = ttk.Entry(self.frame_content, width=24, font=('Arial', 10), background=bg_color)
-        self.text_comments = tkk.Text(self.frame_content, width=50, height=10, font=('Arial', 10), background=bg_color)
+        self.entry_name = ttk.Entry(self.frame_content, width=24, font=('Arial', 10))
+        self.entry_email = ttk.Entry(self.frame_content, width=24, font=('Arial', 10))
+        self.text_comments = tkk.Text(self.frame_content, width=50, height=10, font=('Arial', 10))
 
         self.entry_name.grid(row=1, column=0, padx=5)
         self.entry_email.grid(row=1, column=1, padx=5)
         self.text_comments.grid(row=3, column=0, columnspan=2, padx=5)
 
-        ttk.Button(self.frame_content, text='Submit',
+        ttk.Button(self.frame_content, text=self.short["txt_feedback_screen__send"],
                    command=self.submit).grid(row=4, column=0, padx=5, pady=5, sticky='e')
-        ttk.Button(self.frame_content, text='Clear',
+        ttk.Button(self.frame_content, text=self.short["txt_feedback_screen__clear"],
                    command=self.clear).grid(row=4, column=1, padx=5, pady=5, sticky='w')
 
     def submit(self):
@@ -54,7 +54,7 @@ class PDFFinderFeedback(tkk.Toplevel):
         print('Email: {}'.format(self.entry_email.get()))
         print('Comments: {}'.format(self.text_comments.get(1.0, 'end')))
         self.clear()
-        messagebox.showinfo(title='Feedback', message='Comments Submitted!', parent=self)
+        messagebox.showinfo(title="txt_feedback_screen_feedback", message=self.short["txt_feedback_screen__comments_submitted"], parent=self)
 
     def clear(self):
         self.entry_name.delete(0, 'end')
