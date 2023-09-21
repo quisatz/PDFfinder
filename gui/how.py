aa5ebc98fa1ced735a7abe74e6f9ccf2079fee63
quisatz <borysgolebiowskipl@gmail.com>
2023-09-21 19:17:53 +0200

    update how

--- gui/how.py
+++ gui/how.py
@@ -1,7 +1,7 @@
 import tkinter as tkk
 from tkinter import ttk
 from webbrowser import open_new
-import pyperclip
+
 
 
 class PDFFinderHow(tkk.Toplevel):
@@ -21,22 +21,18 @@ class PDFFinderHow(tkk.Toplevel):
         self.label4 = tkk.Label(self, width=5)
         self.label5 = tkk.Label(self, width=5)
 
-        ttk.Label(self, text=self.short["txt_PDF_finder_information__PDF_finder_ver_1_0_(x64)"] , font='Helvetica 12 bold').grid(row=0, column=0, padx=10, pady=5, columnspan=2 ,sticky='')
+        ttk.Label(self, text=self.short["txt_PDF_finder_information__PDF_finder_ver_1_0_(x64)"] , font='Helvetica 12 bold').grid(row=0, column=0, padx=10, pady=(5,20), columnspan=2 ,sticky='')
 
-        ttk.Label(self, text=self.short["user_manual"]).grid(row=3, column=0, padx=10, pady=5, sticky='sw')
 
-        link1 = ttk.Label(self, text='en', font=('Arial',9,'underline'), cursor="hand2")
-        link1.grid(row=3, column=0, padx=(50,0) )
-        link1.bind("<Button-1>", lambda e: open_new("https://www.facebook.com/NathanCelina"))
 
-        link2 = ttk.Label(self, text='pl', font=('Arial',9,'underline') ,cursor="hand2")
-        link2.grid(row=3, column=0, padx=(100,0))
-        link2.bind("<Button-1>",
-                   lambda e: open_new("https://www.linkedin.com/in/borys-go%C5%82%C4%99biowski-02b883158/"))
+        link1 = ttk.Label(self, text=self.short["user_manual"], font=('Arial',9,'underline'), cursor="hand2")
+        link1.grid(row=3, column=0, padx=10, pady=5, sticky='sw')
+        link1.bind("<Button-1>", lambda e: open_new("Demonstration.gif"))
+
 
         link3 = ttk.Label(self, text=self.short["demonstration_of_the_program_on_youtube"],  font=('Arial',9,'underline') , cursor="hand2")
         link3.grid(row=6, column=0, pady=(10,10), padx=10, sticky='sw')
-        link3.bind("<Button-1>", lambda e: open_new("https://www.youtube.com/"))
+        link3.bind("<Button-1>", lambda e: open_new("https://www.youtube.com/watch?v=LrCqMymx5WE"))
 
 
 
